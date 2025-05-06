82bb91716302b510acb48ee29f1a8cf2b3af4303
M.Sychev <ext-mikhail.sychev@sbermarket.ru>
2025-05-06 23:03:43 +0300

    add postgresql repo

--- internal/app/app.go
+++ internal/app/app.go
@@ -19,16 +19,7 @@ func Run(cfg *config.Config) {
 	}
 	defer conn.Close(context.Background())
 
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		slog.Error("QueryRow failed", slog.String("error", err.Error()))
-		os.Exit(1)
-	}
-
-	slog.Info("Success connect to DB", slog.String("Message from DB", greeting))
-
-	repo := repo2.NewInMemoryUserRepo()
+	repo := repo2.NewPostgreUserRepo(conn)
 	userService := usecase.NewUserService(repo)
 	server := http.NewHttpServer(cfg, userService)
 	http.Serve(server, cfg)
---
+++ internal/repo/postgre-user-repo.go
@@ -0,0 +1,99 @@
+package repo
+
+import (
+	"context"
+	"database/sql"
+	"evrone_go_hw_5_1/internal/entity"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+)
+
+type PostgreUserRepo struct {
+	conn *pgx.Conn
+}
+
+func NewPostgreUserRepo(conn *pgx.Conn) PostgreUserRepo {
+	return PostgreUserRepo{conn: conn}
+}
+
+func (p PostgreUserRepo) Save(user entity.User) (entity.User, error) {
+	var id string
+	query := "INSERT INTO users (name, email, role) VALUES ($1, $2, $3) RETURNING id"
+	err := p.conn.QueryRow(context.Background(), query, user.Name, user.Email, user.Role).Scan(&id)
+
+	if err != nil {
+		return user, fmt.Errorf("ошибка при вставке пользователя в DB: %w", err)
+	}
+	user.ID = id
+
+	return user, nil
+}
+
+func (p PostgreUserRepo) FindByID(id string) (entity.User, error) {
+	query := "SELECT * from users WHERE id = $1"
+	var user entity.User
+
+	// Выполняем запрос и сканируем результат
+	err := p.conn.QueryRow(context.Background(), query, id).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Role,
+		&user.CreatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return entity.User{}, &ErrorUserNotFound{}
+	}
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
+}
+
+func (p PostgreUserRepo) FindAll() ([]entity.User, error) {
+	query := "SELECT * FROM users"
+
+	rows, err := p.conn.Query(context.Background(), query)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка при получении пользователей: %w", err)
+	}
+	defer rows.Close()
+
+	var users []entity.User
+
+	for rows.Next() {
+		var user entity.User
+
+		err := rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Email,
+			&user.Role,
+			&user.CreatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("ошибка при получении пользователей, сканированиe строки: %w", err)
+		}
+
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("ошибка при получении пользователей, ошибка при обработке строк: %w", err)
+	}
+
+	return users, nil
+}
+
+func (p PostgreUserRepo) DeleteByID(id string) error {
+	query := "DELETE FROM users WHERE id = $1"
+
+	_, err := p.conn.Exec(context.Background(), query, id)
+	if err != nil {
+		return fmt.Errorf("ошибка при удалении пользователя: %w", err)
+	}
+
+	return nil
+}
