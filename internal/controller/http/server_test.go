554dc7806c0a56c69c4e21c0d3b2843062abc7bc
M.Sychev <ext-mikhail.sychev@sbermarket.ru>
2025-06-01 17:25:30 +0300

    add tests for server

--- internal/controller/http/server_test.go
+++ internal/controller/http/server_test.go
@@ -9,12 +9,13 @@ import (
 	http2 "evrone_go_hw_5_1/internal/controller/http"
 	"evrone_go_hw_5_1/internal/entity"
 	"evrone_go_hw_5_1/internal/entity/dto"
+	"evrone_go_hw_5_1/internal/repo"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-// MockUserService для тестирования
 type MockUserService struct {
 	CreateUserFunc func(ctx context.Context, name, email string, role entity.UserRole) (entity.User, error)
 	GetUserFunc    func(ctx context.Context, id string) (entity.User, error)
@@ -53,7 +54,6 @@ func (m *MockUserService) RemoveUser(ctx context.Context, id string) error {
 func TestServer_Save(t *testing.T) {
 	cfg := &config.Config{}
 
-	// Успешный кейс
 	t.Run("Success", func(t *testing.T) {
 		mockService := &MockUserService{
 			CreateUserFunc: func(ctx context.Context, name, email string, role entity.UserRole) (entity.User, error) {
@@ -85,18 +85,17 @@ func TestServer_Save(t *testing.T) {
 		server.Save(w, req)
 
 		if w.Code != http.StatusOK {
-			t.Errorf("Ожидаемый статус: %d, полученный: %d", http.StatusOK, w.Code)
+			t.Errorf("Want status: %d, got: %d", http.StatusOK, w.Code)
 		}
 
 		var response dto.UserResponseBody
 		json.Unmarshal(w.Body.Bytes(), &response)
 
 		if response.ID != "1" || response.Name != "Test User" || response.Email != "test@example.com" || response.Role != "user" {
-			t.Errorf("Некорректный ответ: %v", response)
+			t.Errorf("Incorrect answer: %v", response)
 		}
 	})
 
-	// Неверный запрос
 	t.Run("InvalidRequest", func(t *testing.T) {
 		mockService := &MockUserService{}
 		server := http2.NewServer(cfg, mockService)
@@ -142,6 +141,205 @@ func TestServer_Save(t *testing.T) {
 
 		if w.Code != http.StatusInternalServerError {
 			t.Errorf("want status: %d, got: %d", http.StatusInternalServerError, w.Code)
+		}
+	})
+}
+
+func TestFindByID(t *testing.T) {
+	cfg := &config.Config{}
+
+	t.Run("Success", func(t *testing.T) {
+		mockService := &MockUserService{
+			GetUserFunc: func(ctx context.Context, id string) (entity.User, error) {
+				return entity.User{
+					ID:    "1",
+					Name:  "Test User",
+					Email: "test@example.com",
+					Role:  entity.UserRoleUser,
+				}, nil
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("GET", "/users/1", nil)
+		w := httptest.NewRecorder()
+
+		server.FindByID(w, req)
+
+		if w.Code != http.StatusOK {
+			t.Errorf("Status want: %d, got: %d", http.StatusOK, w.Code)
+		}
+
+		var response dto.UserResponseBody
+		json.Unmarshal(w.Body.Bytes(), &response)
+
+		if response.ID != "1" || response.Name != "Test User" || response.Email != "test@example.com" || response.Role != "user" {
+			t.Errorf("Incorrect answer: %v", response)
+		}
+	})
+
+	t.Run("NotFound", func(t *testing.T) {
+		mockService := &MockUserService{
+			GetUserFunc: func(ctx context.Context, id string) (entity.User, error) {
+				return entity.User{}, &repo.ErrorUserNotFound{}
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("GET", "/users/1", nil)
+		w := httptest.NewRecorder()
+
+		server.FindByID(w, req)
+
+		if w.Code != http.StatusNotFound {
+			t.Errorf("Status want: %d, got: %d", http.StatusNotFound, w.Code)
+		}
+	})
+
+	t.Run("InternalError", func(t *testing.T) {
+		mockService := &MockUserService{
+			GetUserFunc: func(ctx context.Context, id string) (entity.User, error) {
+				return entity.User{}, fmt.Errorf("internal error")
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("GET", "/users/1", nil)
+		w := httptest.NewRecorder()
+
+		server.FindByID(w, req)
+
+		if w.Code != http.StatusInternalServerError {
+			t.Errorf("Status want: %d, got: %d", http.StatusInternalServerError, w.Code)
+		}
+	})
+}
+
+func TestServer_FindAll(t *testing.T) {
+	cfg := &config.Config{}
+
+	t.Run("Success", func(t *testing.T) {
+		mockService := &MockUserService{
+			ListUsersFunc: func(ctx context.Context) ([]entity.User, error) {
+				return []entity.User{
+					{
+						ID:    "1",
+						Name:  "User 1",
+						Email: "user1@example.com",
+						Role:  entity.UserRoleUser,
+					},
+					{
+						ID:    "2",
+						Name:  "User 2",
+						Email: "user2@example.com",
+						Role:  entity.UserRoleAdmin,
+					},
+				}, nil
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("GET", "/users", nil)
+		w := httptest.NewRecorder()
+
+		server.FindAll(w, req)
+
+		if w.Code != http.StatusOK {
+			t.Errorf("Status want: %d, got: %d", http.StatusOK, w.Code)
+		}
+
+		var response dto.UsersResponseBody
+		err := json.Unmarshal(w.Body.Bytes(), &response)
+		if err != nil {
+			t.Errorf("Error in test, unmarshall json: %v", err)
+		}
+
+		if len(response.Data) != 2 {
+			t.Errorf("Want 2 users got: %d", len(response.Data))
+		}
+	})
+
+	t.Run("ServiceError", func(t *testing.T) {
+		mockService := &MockUserService{
+			ListUsersFunc: func(ctx context.Context) ([]entity.User, error) {
+				return nil, fmt.Errorf("internal service error")
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("GET", "/users", nil)
+		w := httptest.NewRecorder()
+
+		server.FindAll(w, req)
+
+		if w.Code != http.StatusInternalServerError {
+			t.Errorf("Want status: %d, got: %d", http.StatusInternalServerError, w.Code)
+		}
+	})
+}
+
+func TestDeleteByID(t *testing.T) {
+	cfg := &config.Config{}
+
+	t.Run("Success", func(t *testing.T) {
+		mockService := &MockUserService{
+			RemoveUserFunc: func(ctx context.Context, id string) error {
+				return nil
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("DELETE", "/users/1", nil)
+		w := httptest.NewRecorder()
+
+		server.DeleteByID(w, req)
+
+		if w.Code != http.StatusNoContent {
+			t.Errorf("Status want: %d, got: %d", http.StatusNoContent, w.Code)
+		}
+	})
+
+	t.Run("NotFound", func(t *testing.T) {
+		mockService := &MockUserService{
+			RemoveUserFunc: func(ctx context.Context, id string) error {
+				return &repo.ErrorUserNotFound{}
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("DELETE", "/users/1", nil)
+		w := httptest.NewRecorder()
+
+		server.DeleteByID(w, req)
+
+		if w.Code != http.StatusNotFound {
+			t.Errorf("Status want: %d, got: %d", http.StatusNotFound, w.Code)
+		}
+	})
+
+	t.Run("InternalError", func(t *testing.T) {
+		mockService := &MockUserService{
+			RemoveUserFunc: func(ctx context.Context, id string) error {
+				return fmt.Errorf("internal service error")
+			},
+		}
+
+		server := http2.NewServer(cfg, mockService)
+
+		req := httptest.NewRequest("DELETE", "/users/1", nil)
+		w := httptest.NewRecorder()
+
+		server.DeleteByID(w, req)
+
+		if w.Code != http.StatusInternalServerError {
+			t.Errorf("Status want: %d, got: %d", http.StatusInternalServerError, w.Code)
 		}
 	})
 }
